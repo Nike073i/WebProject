73322e14ea240048982cb03b5783fded1f9de33f
Nike073i <nike073i@mail.ru>
2022-10-21 12:41:38 +0400

    Коммит с исправлением теста

--- WorldOfPowerToolsTest.Test/AdminControllerTests.cs
+++ WorldOfPowerToolsTest.Test/AdminControllerTests.cs
@@ -71,7 +71,7 @@ namespace SportsStore.Tests {
             AdminController target = new AdminController(mock.Object);
 
             // Act
-            Product result = GetViewModel<Product>(target.Edit(3));
+            Product result = GetViewModel<Product>(target.Edit(4));
 
             // Assert
             Assert.Null(result);
